cfa568f6643edce8cecb2ec65b21596853f14a16
Rogério Jordão <ro_jordao@hotmail.com>
2023-02-17 17:28:14 -0300

    Add files via upload

---
+++ HelloWorld/src/br/com/rogsajo/helloworld/HelloWolrd.java
@@ -0,0 +1,13 @@
+package br.com.rogsajo.helloworld;
+
+public class HelloWolrd {
+
+	// Your First Program
+
+	class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	    }
+	}
+	
+}
